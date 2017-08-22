da1c5f2da8d4ef510b610ebda314d447a673bc9e
Tim McWilliams <tmcwilliams@smu.edu>
2017-08-22 01:37:42 -0400

    R script for plotting

--- Output Files/Plots.R
+++ Output Files/Plots.R
@@ -1,23 +1,23 @@
-#Load libary's 
+# Load libary's 
 library("ggplot2") # for creating the plot
 library("reshape2") # needed for melting data together
 
-#Set working directory
+# Set working directory
 setwd("")
 
-#Check files 
+# Check files 
 list.files()
 
-#Read txt file in to R
+# Read txt file in to R
 rs <- read.table("PlottingData.txt")
 
-#Check data
+# Check data
 head(rs)
 
-#Melt the wanted variables together
+# Melt the wanted variables together
 dfm <- melt(rs[,c('Team','TotalRunsScoredRanked','TotalRunsAllowedRanked', 'TotalHomeRunsRanked')],id.vars = 1)
 
-#Plot the data
+# Plot the data
 ggplot(dfm,aes(x = Team,y = value)) + 
   geom_bar(aes(fill = variable),stat = "identity",position = "dodge") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
