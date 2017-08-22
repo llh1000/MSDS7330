bba0dec59c0d07bd049e47a3bcd06c5e3f5efcd3
Tim McWilliams <tmcwilliams@smu.edu>
2017-08-22 01:25:55 -0400

    Contains the R code for plotting

---
+++ Output Files/Plots.R
@@ -0,0 +1,31 @@
+#Load libary's 
+library("ggplot2")
+library("plyr")
+library("reshape2")
+
+#Set working directory
+setwd("")
+
+#Check files 
+list.files()
+
+#Read csv file in to R
+rs <- read.csv("AttendancVSRuns.csv", sep = ",")
+
+#Check data
+head(rs)
+
+#Melt the data wanted variables together
+dfm <- melt(rs[,c('Team','TotalRunsScoredRanked','TotalRunsAllowedRanked', 'TotalHomeRunsRanked')],id.vars = 1)
+
+#Plot the data
+ggplot(dfm,aes(x = Team,y = value)) + 
+  geom_bar(aes(fill = variable),stat = "identity",position = "dodge") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+  labs(x = "Team", y = "Rank")
+  
+
+
+
+
+
