ee2793fbbd19f1a1ef1d1d59ec4f4ed521df18e8
llh1000 <llh1000@yahoo.com>
2017-08-23 11:53:58 -0500

    Update Data Files.sql

--- Query Files/Data Files.sql
+++ Query Files/Data Files.sql
@@ -13,35 +13,6 @@ fields terminated by ",";
 
 select * from attendance;
 
-create table `ballparks`
-(	`TEAM` varchar(3),
-	`FRANCHISE` varchar(35),
-    `NAME` varchar (35),
-    `BALLPARK` varchar (35),
-    `BEGIN_YEAR` int,
-    `END_YEAR` int
-)
-;
-
-LOAD DATA LOCAL INFILE C:/Users/Kelly/Documents/SMU/DATABASE/baseball/ballparks.csv into table ballparks
-fields terminated by ",";
-
-select* from ballparks;
-
-create table `divisions`
-(	`TEAM` varchar(3),
-	`LEAGUE` varchar(35),
-    `DIVISION` varchar (35),
-    `BEGIN_YEAR` int,
-    `END_YEAR` int
-)
-;
-
-LOAD DATA LOCAL INFILE C:/Users/Kelly/Documents/SMU/DATABASE/baseball/divisions.csv into table divisions
-fields terminated by ",";
-
-select* from divisions;
-
 create table `wins`	
 (	`TEAM` varchar(3),
 	`YEAR` numeric,
